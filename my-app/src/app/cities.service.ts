6e144d825c4013d060cfdebaef508fd6f5571c35
LinaLumburovska <lina.lumburovska@gecko.mk>
2019-09-25 11:18:06 +0200

    Removed unused data

--- my-app/src/app/cities.service.ts
+++ my-app/src/app/cities.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { of} from 'rxjs';
+import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 const PARAMS = new HttpParams({
--- my-app/src/app/current-city/current-city.component.ts
+++ my-app/src/app/current-city/current-city.component.ts
@@ -50,14 +50,4 @@ export class CurrentCityComponent implements OnInit {
           });
       });
   }
-
-  convertTime = (unixTimestamp: number): string => {
-    const date = new Date(unixTimestamp * 1000);
-    const hours = date.getHours();
-    const minutes = '0' + date.getMinutes();
-    const seconds = '0' + date.getSeconds();
-    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    return formattedTime;
-  }
-
 }
